79490b7b6c9bdcec164f8a8ef9b31eb7eba047a0
Jacob Kostecki <jacob@kostecki.dk>
2023-03-28 17:47:34 +0200

    add material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,32 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
 import { SessionProvider } from "next-auth/react";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+
+import lightThemeOptions from "../styles/theme/lightThemeOptions";
+import createEmotionCache from "../utils/createEmotionCache";
+
+import "@/styles/globals.css";
+
+interface AppPropsCustom extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const lightTheme = createTheme(lightThemeOptions);
+const clientSideEmotionCache = createEmotionCache();
 
 export default function App({
   Component,
+  emotionCache = clientSideEmotionCache,
   pageProps: { session, ...pageProps },
-}: AppProps) {
+}: AppPropsCustom) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={lightTheme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
     </SessionProvider>
   );
 }
