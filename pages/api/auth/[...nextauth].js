9f83b9e6209730c00398dc832549e70f5824f331
Jacob Kostecki <jacob@kostecki.dk>
2023-03-28 17:47:48 +0200

    return full user profile from untappd

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,22 @@
 import NextAuth from "next-auth";
 
 export default NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user = token.user;
+      }
+
+      return session;
+    },
+    async jwt({ token, user, account }) {
+      if (account) {
+        token.user = user;
+        // token.user.accessToken = account.access_token;
+      }
+      return token;
+    },
+  },
   providers: [
     {
       id: "untappd",
@@ -29,10 +45,32 @@ export default NextAuth({
         const { user } = profile.response;
         return {
           id: user.id,
-          name: user.user_name,
+          username: user.user_name,
           email: user.settings.email_address,
           image: user.user_avatar_hd,
+          firstName: user.first_name,
+          lastName: user.last_name,
+          isSupporter: user.is_supporter,
+          untappdUrl: user.untappd_url,
+          stats: {
+            totalBadges: user.stats.total_badges,
+            totalFriends: user.stats.total_friends,
+            totalCheckins: user.stats.total_checkins,
+            totalBeers: user.stats.total_beers,
+            totalCreatedBeers: user.stats.total_created_beers,
+            totalFollowing: user.stats.total_followings,
+            totalPhotos: user.stats.total_photos,
+          },
+          dateJoined: user.date_joined,
         };
+      },
+      style: {
+        logo: "/github.svg",
+        logoDark: "/github-dark.svg",
+        bg: "#fff",
+        bgDark: "#000",
+        text: "#000",
+        textDark: "#fff",
       },
     },
   ],
