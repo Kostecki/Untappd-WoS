d9ed9fe1f6a2d3aa562e13dad32c103c0bc59135
Jacob Kostecki <jacob@kostecki.dk>
2025-01-20 01:21:34 +0100

    add logout-functionality

--- app/components/auth/login.tsx
+++ app/components/auth/login.tsx
@@ -9,9 +9,12 @@ import {
   Button,
   PasswordInput,
 } from "@mantine/core";
-import { Form } from "react-router";
+import { Form, useNavigation } from "react-router";
 
 export default function Login() {
+  const navigation = useNavigation();
+  const isLoading = ["submitting", "loading"].includes(navigation.state);
+
   return (
     <Container mt="xl" size="480">
       <Card shadow="sm" padding="lg" withBorder>
@@ -30,7 +33,13 @@ export default function Login() {
           <Stack>
             <TextInput name="username" placeholder="Username" required />
             <PasswordInput name="password" placeholder="Password" required />
-            <Button variant="filled" color="#ffc000" type="submit">
+
+            <Button
+              variant="filled"
+              color="#ffc000"
+              type="submit"
+              loading={isLoading}
+            >
               Log in with Untappd
             </Button>
           </Stack>
